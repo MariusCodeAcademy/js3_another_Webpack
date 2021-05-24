1e25140dbf3df9153191040262a531f177cfca94
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-24 13:38:49 +0300

    babel + source map

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: "source-map", // galima matyti is kurio failo koks kodas atejo
   entry: {
     // kuri faila paims webpack kaip pagrindini
     main: path.resolve(__dirname, "./src/app.js"),
@@ -15,9 +16,21 @@ module.exports = {
   },
   module: {
     rules: [
+      // css loader
       {
         test: /\.css$/i, // pritaikom .css failam
         use: ["style-loader", "css-loader"], // uzkraunam css
+      },
+      // babel loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
