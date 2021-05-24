5a4dc0dd93733546d045fba65e566803fd248419
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-24 15:18:24 +0300

    IMG compress

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path"); // node modulis dirbti su keliais iki failu
 // html generavimo pluginas
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -46,6 +47,18 @@ module.exports = {
     ],
   },
   plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["mozjpeg", { quality: 50 }],
+          ["pngquant", { quality: [0.5, 0.7] }],
+          ["svgo"],
+        ],
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./src/html/template.html",
       templateParameters: {
