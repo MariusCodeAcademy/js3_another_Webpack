c88940f352115734f7d860a3776195d2052392f1
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-24 13:17:51 +0300

    output config, clean, mainTitle Var

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,13 +8,18 @@ module.exports = {
     // kuri faila paims webpack kaip pagrindini
     main: path.resolve(__dirname, "./src/app.js"),
   },
-  output: {},
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // isvalom pries tai dist buvusius failus
+  },
   module: {},
   plugins: [
     new HtmlWebpackPlugin({
       template: "/src/html/template.html",
       templateParameters: {
         title: "We now know WebPack.",
+        mainTitle: "This is easy",
       },
       minify: {
         removeComments: true,
